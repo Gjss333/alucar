52874197ccacd37d11e04db6958dc8e6638422bd
Gjss333 <gleydson_jose333@outlook.com>
2023-07-25 18:30:25 -0300

    Criação de metodos para APIREST

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -7,20 +7,21 @@ use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
+
+    //injeção de instancia no model
+    public function __construct(Marca $marca)
+    {
+        $this->marca = $marca;
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
-    }
+        //$marca = Marca::all();
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        $marcas = $this->marca->all();
+        return $marcas;
     }
 
     /**
@@ -28,38 +29,54 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // $marca = Marca::create($request->all());
+        $marcas = $this->marca->create($request->all());
+        return $marcas;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Marca $marca)
+    public function show($id)
     {
-        //
-    }
+        $marcas = $this->marca->find($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Marca $marca)
-    {
-        //
+        if($marcas === null){
+            return ['msg' => 'Recurso pesquisado não existe'];
+        }
+        
+        return $marcas;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Marca $marca)
+    public function update(Request $request, $id)
     {
-        //
+        // $marca->update($request->all());
+        $marcas = $this->marca->find($id);
+        
+        if($marcas === null){
+            return ['msg' => 'impossível realizar a atualização do registro'];
+        }
+        
+        $marcas->update($request->all());
+        return $marcas;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Marca $marca)
+    public function destroy($id)
     {
-        //
+        // $marca->delete();
+        $marcas = $this->marca->find($id);
+          
+        if($marcas === null){
+            return ['msg' => 'impossível a remoção do registro'];
+        }
+        
+        $marcas->delete();
+        return ['msg' => 'removida com sucesso'];
     }
 }
